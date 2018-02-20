12a3d2532422243fde0811747dd3fb38a0f33371
Yj Poh <yjpoh@Yjs-MacBook-Pro.local>
2018-02-20 10:39:48 +0800

    edited controller again

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < ApplicationController
   def index
-      @all_orders = DeliveryOrder.all.as_json
-      render json: @all_orders.as_json(:except => [:order_items])
+    @all_orders = DeliveryOrder.all.as_json
+    render json: @all_orders.as_json(:except => [:order_items])
   end
 
   def show
-      @current_order = DeliveryOrder.find_by(order_id: params[:id]).as_json
+    @current_order = DeliveryOrder.find_by(order_id: params[:id]).as_json
+    unless @current_order == nil
       render json: @current_order
+    else
+      render json: {error: "Order does not exist"}
+    end
   end
 end
